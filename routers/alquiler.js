d4efff289b4d2165e1fc2ad82448e545f6474c9d
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-08-15 21:55:03 -0500

    feat: :rocket: Implementation of middleware dto
    Se le aplicó el middleware del dto para su buen funcionamiento

--- routers/alquiler.js
+++ routers/alquiler.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import con from '../db/atlas.js'
 import { ObjectId } from "bson";
 import { configGET } from "../limit/limit.js";
-import { middlewareVerify } from "../middleware/proxyalquiler.js";
+import { middlewareVerify, proxyAlquiler } from "../middleware/proxyalquiler.js";
+
 
 const appAlquiler = Router();
 
-appAlquiler.get("/get", configGET(), middlewareVerify, async(req, res) => {
+appAlquiler.get("/get", configGET(),proxyAlquiler,  middlewareVerify, async(req, res) => {
     if (!req.rateLimit) return; 
     console.log(req.rateLimit);
 
@@ -17,7 +18,7 @@ appAlquiler.get("/get", configGET(), middlewareVerify, async(req, res) => {
     
 });
 
-appAlquiler.post("/post", configGET(), middlewareVerify,async(req, res) => {
+appAlquiler.post("/post", configGET(), proxyAlquiler, middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; 
     // res.send("hola");
     console.log(req.rateLimit);
@@ -36,16 +37,16 @@ appAlquiler.post("/post", configGET(), middlewareVerify,async(req, res) => {
         res.send({ message: "alquiler ingresado correctamente", result });
     
     } catch (error) {
-        console.log(error.errInfo.details.schemaRulesNotSatisfied);
+        /* console.log(error.errInfo.details.schemaRulesNotSatisfied);
         const propertiesNotSatisfied = error.errInfo.details.schemaRulesNotSatisfied;
         propertiesNotSatisfied.forEach(property => {
             console.log(`Property: ${property.operatorName}`);
             console.log(`Unsatisfied property: ${property.propertyName}`);
             console.log(`Description: ${property.description}`);
             console.log("Details:", property.details);
-        });
-        
-        res.send({ message: "No fue posible ingresar el alquiler" });
+        }); */
+        console.log("Este id ya existe");
+        res.send({ message: "No fue posible ingresar el alquiler, el id ya existe" });
     }
 });
 
@@ -62,7 +63,7 @@ appAlquiler.post("/post", configGET(), middlewareVerify,async(req, res) => {
  */
 
 
-appAlquiler.put("/put/:id",  configGET(), middlewareVerify,async(req, res) => {
+appAlquiler.put("/put/:id",  configGET(),proxyAlquiler,  middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; 
     
     console.log(req.rateLimit);
@@ -93,7 +94,7 @@ appAlquiler.put("/put/:id",  configGET(), middlewareVerify,async(req, res) => {
     
 });
 
-appAlquiler.delete("/delete/:id",  configGET(),  middlewareVerify,async(req, res) => {
+appAlquiler.delete("/delete/:id",  configGET(),proxyAlquiler,   middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; 
     
     console.log(req.rateLimit);
@@ -117,7 +118,7 @@ appAlquiler.delete("/delete/:id",  configGET(),  middlewareVerify,async(req, res
 
 // http://localhost:5022/alquiler/get/Disponible
 // Obtener todos los automóviles disponibles para alquiler.
-appAlquiler.get("/get/:Estado",  configGET(), middlewareVerify,async(req, res) => {
+appAlquiler.get("/get/:Estado",  configGET(), proxyAlquiler, middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; ;
     console.log(req.rateLimit);
     
@@ -133,7 +134,7 @@ appAlquiler.get("/get/:Estado",  configGET(), middlewareVerify,async(req, res) =
 
 
 //Buscar un alquiler por su id
-appAlquiler.get("/search/alq/:id", configGET(), middlewareVerify,async(req, res) => {
+appAlquiler.get("/search/alq/:id", configGET(), proxyAlquiler, middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; ;
     console.log(req.rateLimit);
     
@@ -157,7 +158,7 @@ appAlquiler.get("/search/alq/:id", configGET(), middlewareVerify,async(req, res)
 
 
 // Obtener el costo total de un alquiler específico. 
-appAlquiler.get("/get/costo/:id",configGET(), middlewareVerify,async(req, res) => {
+appAlquiler.get("/get/costo/:id",configGET(), proxyAlquiler, middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; 
     
     console.log(req.rateLimit);
@@ -179,7 +180,7 @@ appAlquiler.get("/get/costo/:id",configGET(), middlewareVerify,async(req, res) =
 
 // Obtener los detalles del alquiler que tiene fecha de inicio en '2023-07-05'.
 
-appAlquiler.get("/get/fecha/:FechaInicio", configGET(), middlewareVerify, async(req, res) => {
+appAlquiler.get("/get/fecha/:FechaInicio", configGET(),proxyAlquiler,  middlewareVerify, async(req, res) => {
     if (!req.rateLimit) return; 
     
     console.log(req.rateLimit);
@@ -200,7 +201,7 @@ appAlquiler.get("/get/fecha/:FechaInicio", configGET(), middlewareVerify, async(
 
 // Obtener los datos de los clientes que realizaron al menos un
 // alquiler.
-appAlquiler.get("/get/clientes/unalq", configGET(),  middlewareVerify,async (req, res) => {
+appAlquiler.get("/get/clientes/unalq", configGET(),proxyAlquiler,   middlewareVerify,async (req, res) => {
     try {
         if (!req.rateLimit) {
             return;
@@ -234,7 +235,7 @@ appAlquiler.get("/get/clientes/unalq", configGET(),  middlewareVerify,async (req
 });
 
 // Obtener la cantidad total de alquileres registrados en la base de datos. 
-appAlquiler.get("/get/totalalquiler/cantidad", configGET(),  middlewareVerify,async(req, res) => {
+appAlquiler.get("/get/totalalquiler/cantidad", configGET(),proxyAlquiler,   middlewareVerify,async(req, res) => {
     if (!req.rateLimit) return; ;
     console.log(req.rateLimit);
     
@@ -257,7 +258,7 @@ appAlquiler.get("/get/totalalquiler/cantidad", configGET(),  middlewareVerify,as
 // .Listar los alquileres con fecha de inicio entre '2023-07-05' y '2023-07-10'.
 
 
-appAlquiler.get("/get/alq/fecha/between/:FechaInicio/:FechaFin", configGET(),  middlewareVerify,async (req, res) => {
+appAlquiler.get("/get/alq/fecha/between/:FechaInicio/:FechaFin", configGET(),proxyAlquiler,   middlewareVerify,async (req, res) => {
     if (!req.rateLimit) {
         return;
     }
