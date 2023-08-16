5614b342a3db57c594f52c91033d48973f7d3cd0
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-08-16 07:00:42 -0500

    feat: :sparkles: Intruduce a new key word for token
    Se le introdujo una nueva palabra para generar el token a la collection

--- limit/token.js
+++ limit/token.js
@@ -8,6 +8,7 @@ import AutomovilDto from "../routers/storage/automovil.js"
 import { Error } from "../routers/storage/mongo.js";
 import ClienteDTO from '../routers/storage/cliente.js';
 import EmpleadoDTO from '../routers/storage/empleado.js';
+import RegistroDevolucionDTO from '../routers/storage/registro_devolucion.js';
 
 
 dotenv.config("../");
@@ -21,8 +22,8 @@ const DTO = (p1) => {
       'automovil': AutomovilDto,
       'cliente': ClienteDTO,
       'empleado': EmpleadoDTO,
-      /* 
-      'registro_devolucion': RegDevolucionDto,
+      'registrodevolucion': RegistroDevolucionDTO,
+      /*
       'registro_entrega': RegEntregaDto,
       'reserva': ReservaDto,
       'sucursal_automovil': SucursalAutoDto,
