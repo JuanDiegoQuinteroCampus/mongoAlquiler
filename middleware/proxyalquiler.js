e055d2f8af5f05c66cf468044d721e3fbd5c0b59
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-08-15 21:54:06 -0500

    feat: :sparkles: Insert DTO to the file proxyalquiler, middleware
    Se realizó un middleware para el funcionamiento del dto en el archivo proxyalquiler

--- middleware/proxyalquiler.js
+++ middleware/proxyalquiler.js
@@ -3,8 +3,32 @@ import {plainToClass, classToPlain} from 'class-transformer';
 import {validate} from 'class-validator';
 import { DTO } from "../limit/token.js";
 import { Router } from "express";
+import express from "express";
+import AlquilerDto from "../routers/storage/alquiler.js";
+
 const middlewareVerify = Router();
 const DTOData = Router();
+const proxyAlquiler = express();
+
+
+// Proxydto alquiler, typescript
+proxyAlquiler.use(async (req, res, next) => {
+    try {
+      const data = plainToClass(AlquilerDto, req.body, { excludeExtraneousValues: true });
+      const validationErrors = await validate(data);
+      if (validationErrors.length > 0) {
+        const errors = validationErrors.map((err) => Object.values(err.constraints));
+        res.status(400).json({ message: "Validation error", errors });
+      } else {
+        req.body = JSON.parse(JSON.stringify(data));
+        next();
+      }
+    } catch (error) {
+      console.error("Internal server error:", error);
+      res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+  });
+
 
 
 middlewareVerify.use((req, res, next) => {
@@ -27,7 +51,8 @@ middlewareVerify.use((req, res, next) => {
         Fecha_Inicio: new Date(payload.Fecha_Inicio),
         Fecha_Fin: new Date(payload.Fecha_Fin)
     };
-
+    console.log(payload);
+    console.log(Clone);
     // Comparar los objetos sin espacios en blanco
     const Verify = JSON.stringify(Clone).replace(/\s+/g, '') === JSON.stringify(payloadDateObjects).replace(/\s+/g, '');
 
@@ -58,5 +83,6 @@ DTOData.use( async(req,res,next) => {
 
 export {
     middlewareVerify,
-    DTOData
+    DTOData,
+    proxyAlquiler
 };
